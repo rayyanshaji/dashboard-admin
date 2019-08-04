799d53ce75f1a92f45130e1f11dfc1c7ee29cf31
Mohammed Rayyan Shaji <rayyanz95@gmail.com>
2019-08-05 03:01:15 +0530

    Added Pie chart

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,17 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  public pieChartData: number[] = [250, 350, 720];
+  public pieChartLabels: string[] = ['UberEats', 'Zomato', 'Swiggy'];
+  public colors: any[] = [
+    {
+      backgroundColor: ['#45AC11', '#DA1212', '#FD5A09'],
+      borderColor: '#111'
+    }
+  ];
+
+  public pieChartType = 'doughnut';
+
   ngOnInit() {
   }
 
